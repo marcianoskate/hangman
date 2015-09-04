49641da645808cf6bf9d3cc10595150ae58c6183
Carlos Andres Giraldo Palacio <carlos.giraldo@mail.mcgill.ca>
2015-09-03 20:18:11 -0400

    Added validations for the Hangman service

--- src/main/java/co/cagiral/service/HangmanImpl.java
+++ src/main/java/co/cagiral/service/HangmanImpl.java
@@ -23,18 +23,42 @@ public class HangmanImpl implements Hangman {
     @Override
     public boolean validateGuess(String secretWord, String guess) {
 
-        if (1 <= guess.length()) {
-            return validateLetter(secretWord, guess);
+        if (secretWord == null) {
+            throw new IllegalArgumentException("Secret word can't be null");
+        }
+        if (guess == null || guess.isEmpty()) {
+            throw new IllegalArgumentException("The guess can't be null or empty");
+        }
+
+        guess = guess.trim();
+
+        if (1 == guess.length()) {
+            return validateLetter(secretWord, guess.charAt(0));
         } else {
             return validateWord(secretWord, guess);
         }
     }
 
-    private boolean validateLetter(String secretWord, String guess) {
-        return false;
+    /**
+     * Validates that the guess is a letter that belongs to the secretWord.
+     * It assumes that both parameters have been validated and are not null.
+     * @param secretWord The word that the player has to guess
+     * @param guess The letter that the player input
+     * @return True if the letter belongs to the word, false otherwise
+     */
+    private boolean validateLetter(String secretWord, char guess) {
+
+        return secretWord.indexOf(guess) >= 0 ;
     }
 
+    /**
+     * Validates that the guess is the secretWord.
+     * It assumes thaty both parameters have been validated and are not null.
+     * @param secretWord The word that the player has to guess
+     * @param guess The word that the player input
+     * @return True if the let
+     */
     private boolean validateWord(String secretWord, String guess) {
-        return false;
+        return secretWord.equalsIgnoreCase(guess);
     }
 }
--- src/test/java/co/cagiral/dao/Hangman.java
+++ src/test/java/co/cagiral/dao/Hangman.java
@@ -20,9 +20,42 @@ public class Hangman {
         instance = new HangmanImpl();
     }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void null_secret_word_throws_exception() {
+        instance.validateGuess(null, "a");
+        instance.validateGuess(null, secretWord);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void null_guess_throws_exception() {
+        instance.validateGuess(secretWord, null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void null_values_trhrows_exception() {
+        instance.validateGuess(null, null);
+    }
+
     @Test
     public void valid_letter_returns_true() {
 
         assertTrue(instance.validateGuess(secretWord, "z"));
+    }
+
+    @Test
+    public void non_valid_letter_returns_false() {
+
+        assertFalse(instance.validateGuess(secretWord, "a"));
+    }
+
+    @Test
+    public void valid_guess_returns_true() {
+
+        assertTrue(instance.validateGuess(secretWord, secretWord));
+    }
+
+    @Test
+    public void invalid_guess_returns_false() {
+        assertFalse(instance.validateGuess(secretWord, "fuzzyi"));
     }
 }
