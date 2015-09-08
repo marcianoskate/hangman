fac3420aff538ac508ba52c72c84c8ea583b4045
Carlos Andres Giraldo Palacio <carlos.giraldo@mail.mcgill.ca>
2015-09-08 12:26:51 -0400

    Created the implementations of the Body parts of the Hangman

---
+++ src/main/java/co/cagiral/view/model/drawing/HangmanBody.java
@@ -0,0 +1,58 @@
+package co.cagiral.view.model.drawing;
+
+/**
+ * Representation of the HangmanStick.
+ *
+ * This class is in charge of drawing the Stick where the Hangman will hang
+ */
+public class HangmanBody extends HangmanBodyPart {
+
+    private HangmanBodyPart leftEye;
+    private HangmanBodyPart rightEye;
+    private HangmanBodyPart nose;
+    private HangmanBodyPart mouth;
+
+    @Override
+    public void addLeftEye(HangmanBodyPart eye) {
+        this.leftEye = eye;
+    }
+
+    @Override
+    public void addRightEye(HangmanBodyPart eye) {
+        this.rightEye = eye;
+    }
+
+    @Override
+    public void addNose(HangmanBodyPart hangmanNose) {
+        this.nose = hangmanNose;
+    }
+
+    @Override
+    public void addMouth(HangmanMouth hangmanMouth) {
+        this.mouth = hangmanMouth;
+    }
+
+    @Override
+    public String draw() {
+        StringBuilder stick = new StringBuilder();
+        stick
+                .append(" |          /~~~\\").append("\n")
+                .append(" |          |").append(eye(leftEye)).append(" ").append(eye(rightEye)).append("|").append("\n")
+                .append(" |          | ").append(aNose(this.nose)).append(" |").append("\n")
+                .append(" |          \\_").append(aMouth(this.mouth)).append("_/").append("\n")
+        ;
+        return stick.toString();
+    }
+
+    private String eye(HangmanBodyPart leftEye) {
+        return leftEye == null ? " " : leftEye.draw();
+    }
+
+    private String aNose(HangmanBodyPart nose) {
+        return nose == null ? " " : nose.draw();
+    }
+
+    private String aMouth(HangmanBodyPart mouth) {
+        return mouth == null ? "_" : mouth.draw();
+    }
+}
---
+++ src/main/java/co/cagiral/view/model/drawing/HangmanDrawTest.java
@@ -0,0 +1,75 @@
+package co.cagiral.view.model.drawing;
+
+import co.cagiral.view.model.drawing.*;
+
+/**
+ * Created by cpalacio on 9/8/2015.
+ */
+public class HangmanDrawTest {
+
+//    public static void main(String[] args) {
+//
+////        byHand();
+//        usingBuilder();
+//    }
+//
+//    private static void usingBuilder() {
+//
+//        System.out.println("*************");
+//        System.out.println("drawing 10 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(10).draw();
+//        System.out.println("drawing 9 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(9).draw();
+//        System.out.println("drawing 8 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(8).draw();
+//        System.out.println("drawing 7 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(7).draw();
+//        System.out.println("drawing 6 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(6).draw();
+//        System.out.println("drawing 5 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(5).draw();
+//        System.out.println("drawing 4 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(4).draw();
+//        System.out.println("drawing 3 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(3).draw();
+//        System.out.println("drawing 2 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(2).draw();
+//        System.out.println("drawing 1 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(1).draw();
+//        System.out.println("drawing 0 attemmpts");
+//        HangmanDrawing.getHangmanDrawing(0).draw();
+//    }
+//
+//    private static void byHand() {
+//        HangmanBodyPart bodyParts = new HangmanStick();
+//        System.out.println("*************");
+//        System.out.println("drawing stick");
+//        bodyParts.draw();
+//
+//        System.out.println("************");
+//        System.out.println("drawing Head");
+//        bodyParts.addHead(new HangmanHead());
+//        bodyParts.draw();
+//
+//        System.out.println("************");
+//        System.out.println("drawing Head with left eye");
+//        bodyParts.addLeftEye(new HangmanEye());
+//        bodyParts.draw();
+//
+//        System.out.println("************");
+//        System.out.println("drawing Head with both eyes");
+//        bodyParts.addRightEye(new HangmanEye());
+//        bodyParts.draw();
+//
+//        System.out.println("************");
+//        System.out.println("drawing Head with a Nose");
+//        bodyParts.addNose(new HangmanNose());
+//        bodyParts.draw();
+//
+//        System.out.println("************");
+//        System.out.println("drawing Head with a Mouth");
+//        bodyParts.addMouth(new HangmanMouth());
+//        bodyParts.draw();
+//    }
+}
+
