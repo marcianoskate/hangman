a86b85994fbb42880e6e820e93b2d66959835cf4
carlos.giraldo <marcianoskate@gmail.com>
2015-09-07 21:58:32 -0400

    Created HangmanViewFactory to create

--- src/main/java/co/cagiral/view/HangmanViewFactory.java
+++ src/main/java/co/cagiral/view/HangmanViewFactory.java
@@ -1,11 +1,17 @@
 package co.cagiral.view;
 
+import co.cagiral.view.awt.HangmanSwing;
 import co.cagiral.view.console.HangmanConsole;
 
 public class HangmanViewFactory {
 
 	public static HangmanView console() {
 		return new HangmanConsole();
+	}
+
+	public static HangmanView swing() {
+
+		return new HangmanSwing();
 	}
 
 }
