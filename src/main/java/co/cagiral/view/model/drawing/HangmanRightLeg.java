ed9b2a5a26559ac487bba793a5294ab6e47cc586
Carlos Andres Giraldo Palacio <carlos.giraldo@mail.mcgill.ca>
2015-09-08 12:41:26 -0400

    Created the HangmanRightLeg

---
+++ src/main/java/co/cagiral/view/model/drawing/HangmanRightLeg.java
@@ -0,0 +1,25 @@
+package co.cagiral.view.model.drawing;
+
+/**
+ * Created by cpalacio on 9/8/2015.
+ */
+public class HangmanRightLeg extends HangmanBodyPart {
+
+    //        //Left Leg
+//        System.out.println("|           /");
+//        System.out.println("|          / ");
+//        System.out.println("|         /  ");
+
+
+    @Override
+    public String draw() {
+        StringBuilder stick = new StringBuilder();
+        stick
+                .append(" |           /").append("\n")
+                .append(" |          / ").append("\n")
+                .append(" |         /  ").append("\n")
+                ;
+
+        return stick.toString();
+    }
+}
