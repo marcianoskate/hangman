5c1cff631063b94695baa1e1cd5bd1409f149e93
Carlos Andres Giraldo Palacio <carlos.giraldo@mail.mcgill.ca>
2015-09-08 12:32:30 -0400

    Created the definition of the Right Arm

---
+++ src/main/java/co/cagiral/view/model/drawing/HangmanRightArm.java
@@ -0,0 +1,26 @@
+package co.cagiral.view.model.drawing;
+
+/**
+ * Created by cpalacio on 9/8/2015.
+ */
+public class HangmanRightArm extends HangmanBodyPart {
+
+    //        //Left Arm
+//        System.out.println("|           /|");
+//        System.out.println("|          / |");
+//        System.out.println("|         /  |");
+
+
+    @Override
+    public String draw() {
+        StringBuilder stick = new StringBuilder();
+        stick
+                .append(" |           /|").append("\n")
+                .append(" |          / |").append("\n")
+                .append(" |         /  |").append("\n")
+                .append(" |            |").append("\n")
+                ;
+
+        return stick.toString();
+    }
+}
